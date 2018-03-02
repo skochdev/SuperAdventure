74ddecd9848a1bcc83340ea9ffea24f199ea8b6a
Bohdan Skochii <b.skochii@gmail.com>
2018-03-02 03:48:10 +0100

    Create collections for quests, loottables and player inventory

--- Engine/Monster.cs
+++ Engine/Monster.cs
@@ -13,6 +13,7 @@ namespace Engine
         public int MaximumDamage { get; set; }
         public int RewardExperiencePoints { get; set; }
         public int RewardGold { get; set; }
+        public List<LootItem> LootTable { get; set; }
 
         public Monster(int id, string name, int maximumDamage, int rewardExperiencePoints,
             int rewardGold, int currentHitPoints, int maximumHitPoints) 
@@ -23,6 +24,7 @@ namespace Engine
             MaximumDamage = maximumDamage;
             RewardExperiencePoints = rewardExperiencePoints;
             RewardGold = rewardGold;
+            LootTable = new List<LootItem>();
         }
     }
 }
